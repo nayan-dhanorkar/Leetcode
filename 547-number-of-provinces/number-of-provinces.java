252dbcfddb033aa3ab96c23a4d0466e1a897bbb3
Nayan Dhanorkar <148746525+nayan-dhanorkar@users.noreply.github.com>
2026-03-18 14:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (83.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,16 +1,26 @@
 class Solution {
-    public void bfs(int i,int[][] isConnected,boolean[] arr){
-        Queue<Integer> que = new LinkedList<>();
-        int n = isConnected.length;
-        arr[i]=true;
-        que.add(i);
-        while(que.size()>0){
-            int front = que.remove();
-            for(int j=0;j<n;j++){
-                if(!arr[j] && isConnected[front][j]==1){
-                    arr[j]=true;
-                    que.add(j);
-                }
+    // public void bfs(int i,int[][] isConnected,boolean[] arr){
+    //     Queue<Integer> que = new LinkedList<>();
+    //     int n = isConnected.length;
+    //     arr[i]=true;
+    //     que.add(i);
+    //     while(que.size()>0){
+    //         int front = que.remove();
+    //         for(int j=0;j<n;j++){
+    //             if(!arr[j] && isConnected[front][j]==1){
+    //                 arr[j]=true;
+    //                 que.add(j);
+    //             }
+    //         }
+    //     }
+    // }
+
+    public void dfs(int i, int[][] adj,boolean[] vis){
+        int n = adj.length;
+        vis[i]=true;
+        for(int j=0;j<n;j++){
+            if(adj[i][j]==1 && !vis[j]){
+                dfs(j,adj,vis);
             }
         }
     }
@@ -20,7 +30,8 @@ class Solution {
         boolean[] arr = new boolean[n];
         for(int i=0;i<n;i++){
             if(arr[i]==false){
-                bfs(i,isConnected,arr);
+                // bfs(i,isConnected,arr);
+                dfs(i,isConnected,arr);
                 count++;
             }
         }
