79c937f92799ed893c212fc5ea311fe0ade1442c
Nayan Dhanorkar <148746525+nayan-dhanorkar@users.noreply.github.com>
2026-03-18 19:17:47 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (94.58%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.java
+++ 871-keys-and-rooms/keys-and-rooms.java
@@ -1,24 +1,35 @@
 class Solution {
-    public void bfs(int i,List<List<Integer>> rooms,boolean[] vis){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(i);
-        while(q.size()>0){
-            int front = q.remove();
-            int n = rooms.get(front).size();
+    // public void bfs(int i,List<List<Integer>> rooms,boolean[] vis){
+    //     Queue<Integer> q = new LinkedList<>();
+    //     q.add(i);
+    //     while(q.size()>0){
+    //         int front = q.remove();
+    //         int n = rooms.get(front).size();
+    //         for(int j=0;j<n;j++){
+    //             int nextroom = rooms.get(front).get(j);
+    //             if(!vis[nextroom]){
+    //                 vis[nextroom]=true;
+    //                 q.add(nextroom);
+    //             }
+    //         }
+    //     }
+    // }
+    public void dfs(int i,List<List<Integer>> rooms,boolean[] vis){
+            int n = rooms.get(i).size();
+            vis[i]=true;
             for(int j=0;j<n;j++){
-                int nextroom = rooms.get(front).get(j);
+                int nextroom = rooms.get(i).get(j);
                 if(!vis[nextroom]){
-                    vis[nextroom]=true;
-                    q.add(nextroom);
+                    dfs(nextroom,rooms,vis);
                 }
             }
-        }
     }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n = rooms.size();
         boolean[] vis = new boolean[n];
         vis[0]=true;
-        bfs(0,rooms,vis);
+        // bfs(0,rooms,vis);
+        dfs(0,rooms,vis);
         for(boolean ele : vis){
             if(ele == false) return false;
         }
