77495aecd4df9c6f5eb05fa4568c290a559a4530
Nayan Dhanorkar <148746525+nayan-dhanorkar@users.noreply.github.com>
2026-03-30 19:45:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (83.98%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,23 +1,38 @@
 class Solution {
     static boolean ans;
-    public void bfs(int i,int[][] adj,int[] visited){
-        Queue<Integer> q = new LinkedList<>();
-        visited[i]=0;
-        q.add(i);
-        while(q.size()>0){
-            int front = q.remove();
-            int color = visited[front];
-            for(int ele : adj[front]){
-                if(visited[ele]==visited[front]){
-                    ans = false;
-                    return;
+    // public void bfs(int i,int[][] adj,int[] visited){
+    //     Queue<Integer> q = new LinkedList<>();
+    //     visited[i]=0;
+    //     q.add(i);
+    //     while(q.size()>0){
+    //         int front = q.remove();
+    //         int color = visited[front];
+    //         for(int ele : adj[front]){
+    //             if(visited[ele]==visited[front]){
+    //                 ans = false;
+    //                 return;
+    //             }
+    //             if(visited[ele]==-1){
+    //                 q.add(ele);
+    //                 visited[ele]=1-color;
+    //             }
+    //         }
+    //     }
+    // }
+
+    public boolean dfs(int i,int[][] adj,int[] visited,int color){
+            visited[i] = color;
+            for(int ele : adj[i]){
+                if(visited[ele]==color){
+                    return false;
                 }
                 if(visited[ele]==-1){
-                    q.add(ele);
-                    visited[ele]=1-color;
+                    if(!dfs(ele,adj,visited,1-color)){
+                        return false;
+                    }
                 }
             }
-        }
+            return true;
     }
 
     public boolean isBipartite(int[][] adj) {
@@ -25,10 +40,13 @@ class Solution {
         int n = adj.length;
         int[] visited = new int[n];
         Arrays.fill(visited,-1);
-        for(int i=0;i<n;i++){
-            if(ans == false) return ans;
-            if(visited[i]==-1) bfs(i,adj,visited);
+        for (int i = 0; i < n; i++) {
+        if (visited[i] == -1) {
+            if (!dfs(i, adj, visited, 0)) {
+                return false;
+            }
         }
-        return ans;
+    }
+    return true;
     }
 }
