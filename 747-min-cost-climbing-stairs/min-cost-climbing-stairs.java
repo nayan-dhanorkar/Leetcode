723d97aff3008fa16f2a5f3692e00cbe891b3b95
Nayan Dhanorkar <148746525+nayan-dhanorkar@users.noreply.github.com>
2026-04-10 17:25:17 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (79.49%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,15 +1,12 @@
 class Solution {
-    // Recursive Top Down - O(2^n) Time Limit Exceeded
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return Math.min(minCost(cost, n-1,dp), minCost(cost, n-2,dp));
-    }
-    private int minCost(int[] cost, int n,int[] dp) {
-        if (n < 0) return 0;
-        if (n==0 || n==1) return cost[n];
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = cost[n] + Math.min(minCost(cost, n-1,dp), minCost(cost, n-2,dp));
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
     }
 }
