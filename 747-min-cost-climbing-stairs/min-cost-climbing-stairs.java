fb0d2a7e9c1c2d44e7d141a23615932720c07277
Nayan Dhanorkar <148746525+nayan-dhanorkar@users.noreply.github.com>
2026-07-26 11:46:10 +0530

    Time: 1 ms (39.38%) | Memory: 45 MB (23.04%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,12 +1,13 @@
 class Solution {
+    public int climb(int[] cost,int i , int[] dp){
+        if(i<=1) return cost[i];
+        if(dp[i]!=-1) return dp[i];
+        return dp[i] = cost[i] + Math.min(climb(cost,i-1,dp),climb(cost,i-2,dp));
+    }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         int[] dp = new int[n];
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        for(int i=2;i<n;i++){
-            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
-        }
-        return Math.min(dp[n-1],dp[n-2]);
+        Arrays.fill(dp,-1);
+        return Math.min(climb(cost,n-1,dp),climb(cost,n-2,dp    ));
     }
 }
