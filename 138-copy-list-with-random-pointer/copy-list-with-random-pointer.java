48e2bf2ffb0b33b28ec6088d553ca806596d9d29
Nayan Dhanorkar <148746525+nayan-dhanorkar@users.noreply.github.com>
2026-08-06 17:19:14 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (14.86%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -5,51 +5,54 @@ class Solution {
         Node t2 = head2;
         while(t1!=null){
             Node temp = new Node(t1.val);
-            t2.next = temp;
-            t2=t2.next;
+            t2.next=temp;
             t1=t1.next;
+            t2=t2.next;
         }
         return head2;
     }
-    public void connextAlternatively(Node head,Node head2){
+    public void connect(Node head,Node head2){
+        Node dummy = new Node(-1);
+        Node temp = dummy;
         Node t1 = head;
         Node t2 = head2;
-        Node dummy = new Node(-1);
-        Node t = dummy;
         while(t1!=null && t2!=null){
-            t.next=t1;
+            temp.next=t1;
             t1=t1.next;
-            t=t.next;
-            t.next=t2;
+            temp=temp.next;
+            temp.next=t2;
             t2=t2.next;
-            t=t.next;
+            temp=temp.next;
         }
     }
-    public void AssignRandom(Node head,Node head2){
+
+    public void random(Node head,Node head2){
         Node t1 = head;
         Node t2 = head2;
         while(t1!=null){
             t2=t1.next;
-            if(t1.random!=null) t2.random=t1.random.next;
-            t1=t1.next.next;
+            if(t1.random!=null) t2.random = t1.random.next;
+            t1 = t1.next.next;
+
         }
     }
-    public void split(Node head, Node head2){
+    public void split(Node head,Node head2){
         Node t1 = head;
         Node t2 = head2;
         while(t1!=null){
-            t1.next=t2.next;
-            t1=t1.next;
-            if(t1==null) break;
-            t2.next=t1.next;
-            t2=t2.next;
+            t1.next = t2.next;
+            t1 = t1.next;
+            if(t1 == null) break;
+            t2.next = t1.next;
+            t2= t2.next;
         }
     }
+
     public Node copyRandomList(Node head) {
         if(head==null) return null;
         Node head2 = deepCopy(head);
-        connextAlternatively(head,head2);
-        AssignRandom(head,head2);
+        connect(head,head2);
+        random(head,head2);
         split(head,head2);
         return head2;
     }
