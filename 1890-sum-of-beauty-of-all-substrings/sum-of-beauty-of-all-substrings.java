42367c653ce23a0a16319d185d9fbff0d0b98e3f
Nayan Dhanorkar <148746525+nayan-dhanorkar@users.noreply.github.com>
2026-08-13 22:51:47 +0530

    Time: 557 ms (9.23%) | Memory: 46.5 MB (27.43%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,27 +1,59 @@
+// class Solution {
+//     public int beautySum(String s) {
+//         int ans =0;
+//         int n = s.length();
+//         for(int i=0;i<n;i++){
+//             HashMap<Character,Integer> map = new HashMap<>();
+//             for(int j=i;j<n;j++){
+//                 char ch = s.charAt(j);
+//                 if(map.containsKey(ch)){
+//                     int freq = map.get(ch);
+//                     map.put(ch,freq+1);
+//                 }
+//                 else{
+//                     map.put(ch,1);
+//                 }
+//                 int min = Integer.MAX_VALUE;
+//                 int max = Integer.MIN_VALUE;
+//                 for(int freq : map.values()){
+//                     min = Math.min(min,freq);
+//                     max = Math.max(max,freq);
+//                 }
+//                 ans += max-min;
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int beautySum(String s) {
-        int ans =0;
+        int ans = 0;
         int n = s.length();
-        for(int i=0;i<n;i++){
-            HashMap<Character,Integer> map = new HashMap<>();
-            for(int j=i;j<n;j++){
+
+        for (int i = 0; i < n; i++) {
+
+            HashMap<Character, Integer> map = new HashMap<>();
+
+            for (int j = i; j < n; j++) {
+
+                // Add current character
                 char ch = s.charAt(j);
-                if(map.containsKey(ch)){
-                    int freq = map.get(ch);
-                    map.put(ch,freq+1);
-                }
-                else{
-                    map.put(ch,1);
-                }
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+
                 int min = Integer.MAX_VALUE;
                 int max = Integer.MIN_VALUE;
-                for(int freq : map.values()){
-                    min = Math.min(min,freq);
-                    max = Math.max(max,freq);
+
+                // Find minimum and maximum frequency
+                for (int freq : map.values()) {
+                    min = Math.min(min, freq);
+                    max = Math.max(max, freq);
                 }
-                ans += max-min;
+
+                // Beauty = max frequency - min frequency
+                ans += max - min;
             }
         }
+
         return ans;
     }
 }
